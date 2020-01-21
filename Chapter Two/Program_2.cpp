d429a52c769f6e309568ea3572a37f1d52c1afcc
Stefan Diminet <Stefan@stefans-mbp.myfiosgateway.com>
2020-01-20 19:15:28 -0500

    Yes

---
+++ Chapter Two/Program_2.cpp
@@ -0,0 +1,16 @@
+// Program #2 - Using a variable
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x; // this declares a variable
+
+    x = 1023; // this assigns 1023 to x
+
+    cout << "This program prints the value of x: ";
+    cout << x; //this displays 1023
+
+    return 0;
+}
